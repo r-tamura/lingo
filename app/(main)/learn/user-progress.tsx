8c7734cfd3d62882187d25aeaa54fa2bb3213be2
r-tamura <rtamura30@gmail.com>
2024-04-06 04:42:06 +0000

    Learn Page Wrappers - fixed vs sticky fixedはviewportに対してstickyは親要素に対して - justify-betweenをつかった中央寄せtips 空要素を一つ追加する

---
+++ app/(main)/learn/user-progress.tsx
@@ -0,0 +1,62 @@
+import { Button } from "@/components/ui/button";
+import { InfinityIcon } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+type Props = {
+	activeCourse: { imageSrc: string; title: string }; // Replace with DB types
+	hearts: number;
+	points: number;
+	hasActiveSubscription: boolean;
+};
+
+export const UserProgress = ({
+	activeCourse,
+	points,
+	hearts,
+	hasActiveSubscription,
+}: Props) => {
+	return (
+		<div className="flex items-center justify-between gap-x-2 w-full">
+			<Link href="/courses">
+				<Button variant={"ghost"}>
+					<Image
+						src={activeCourse.imageSrc}
+						alt={activeCourse.title}
+						className="rouded-md border"
+						width={28}
+						height={28}
+					/>
+				</Button>
+			</Link>
+			<Link href="/shop">
+				<Button variant={"ghost"} className="text-orange-500">
+					<Image
+						src="/points.svg"
+						alt="Points"
+						className="rouded-md"
+						width={28}
+						height={28}
+					/>
+					{points}
+				</Button>
+			</Link>
+			<Link href="/shop">
+				<Button variant={"ghost"} className="text-orange-500">
+					<Image
+						src="/heart.svg"
+						alt="Hearts"
+						className="rouded-md"
+						width={22}
+						height={22}
+					/>
+					{hasActiveSubscription ? (
+						<InfinityIcon className="h-4 w-4 stroke-[3]" />
+					) : (
+						hearts
+					)}
+				</Button>
+			</Link>
+		</div>
+	);
+};
